82bb00349cf43daaf9354bfd4ade0485c56c04ad
jimmy8646 <yujiechueh@gmail.com>
2018-08-25 12:33:18 +0800

    doc: readme 版本號以及註解slideshow避免gulp噴掉

--- src/js/slideshow.js
+++ src/js/slideshow.js
@@ -1,15 +1,15 @@
-import 'bxslider';
+// import 'bxslider';
 
-$(function() {
-    $('.bxslider').bxSlider({
-        mode: 'horizontal',
-        captions: true,
-        adaptiveHeight: true,
-        pager: false,
-    });
-    $(window).resize(() => {
-        $('.bxslider').bxSlider('reloadSlider');
-    });
-});
+// $(function() {
+//     $('.bxslider').bxSlider({
+//         mode: 'horizontal',
+//         captions: true,
+//         adaptiveHeight: true,
+//         pager: false,
+//     });
+//     $(window).resize(() => {
+//         $('.bxslider').bxSlider('reloadSlider');
+//     });
+// });
 
 // export default test();
