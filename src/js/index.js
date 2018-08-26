d78c151c1c2e989df3885c512840a3153ef79b04
jimmy8646 <yujiechueh@gmail.com>
2018-08-26 13:36:24 +0800

    feat: header 待確認icon 調整

--- src/js/index.js
+++ src/js/index.js
@@ -14,25 +14,3 @@ import 'lightbox2/dist/css/lightbox.min.css'
 window.addEventListener('load', () => {
     AOS.init()
 })
-
-const footerFixed = () => {
-
-  let newDOM = document.createElement('div')
-  let footerDOM = document.getElementById('footer')
-
-  footerDOM.style.width = '100%'
-  footerDOM.style.position = 'fixed'
-  footerDOM.style.bottom = '0'
-
-  newDOM.style.height = `${footerDOM.offsetHeight}px`
-
-  window.addEventListener('resize', () => {
-    newDOM.style.height = `${footerDOM.offsetHeight}px`
-  })
-
-  document.body.insertBefore( newDOM, document.getElementById('footer'))
-}
-
-window.addEventListener('load', () => {
-  footerFixed()
-})
