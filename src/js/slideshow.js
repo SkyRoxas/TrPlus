dfedbe754b78bacc0731bb72738b4cc8dad78ae9
jimmy8646 <yujiechueh@gmail.com>
2018-08-27 22:57:21 +0800

    style: header 手機版

--- src/js/slideshow.js
+++ src/js/slideshow.js
@@ -3,6 +3,17 @@ import 'slick-carousel';
 $(function() {
   $('#slideshow').slick({
     dots: true,
+    autoplay: true,
+    mobileFirst:true,
+    arrows:false,
+    responsive:[
+      {
+        breakpoint: 575,
+        settings: {
+          arrows:true
+      }
+      }
+    ]
   })
 })
 
