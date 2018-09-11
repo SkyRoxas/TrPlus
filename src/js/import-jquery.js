218565694665560830a7d6c5d96fab28d9760b6e
skyroxas <skyroxas1521@gmail.com>
2018-09-11 23:43:52 +0800

    新增商品內頁收合效果

--- src/js/import-jquery.js
+++ src/js/import-jquery.js
@@ -5,12 +5,24 @@ import $ from 'jquery'
 export default window.$ = window.jQuery = $
 
 
-$(document).ready(function(){
+$(document).ready(function() {
   $('#fixed-menu-toggle').on('click', function() {
-    $( '#fixed-menu' ).slideToggle()
+    $('#fixed-menu').slideToggle()
   })
 
-  $('.menu-xs__close-icon').on('click',function(){
+  $('.menu-xs__close-icon').on('click', function() {
     $('#fixed-menu-toggle').trigger('click')
   })
+
+  $('.card05').each(function() {
+    let that = this
+    $(this).find('.title').on('click', function() {
+      $(that).find('.title').toggleClass('show')
+      $(that).find('.body').slideToggle()
+    })
+  })
+
+
+
+
 })
